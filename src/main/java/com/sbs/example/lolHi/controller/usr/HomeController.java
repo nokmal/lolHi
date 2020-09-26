032aa5355820fe3d05c62aa3eeec8d6e120b08ea
nokmal <9aedarae@gmail.com>
2020-09-26 16:39:06 +0900

    web 프로젝트 수업

---
+++ src/main/java/com/sbs/example/lolHi/controller/usr/HomeController.java
@@ -0,0 +1,44 @@
+package com.sbs.example.lolHi.controller.usr;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+//브라우저의 요청을 처리할 수 있음
+
+@Controller
+public class HomeController {
+	@RequestMapping("/usr/home/main")
+	@ResponseBody
+	public String showMain() {
+		return "안녕";
+	}
+	
+	@RequestMapping("/usr/home/main2")
+	@ResponseBody
+	public String showMain2() {
+		return "잘가";
+	}
+	
+	// /usr/home/plus?a=10&b=15
+	@RequestMapping("/usr/home/plus")
+	@ResponseBody
+	public int showPlus(int a, int b) {
+		return a+b;
+	}
+	@RequestMapping("/usr/home/minus")
+	@ResponseBody
+	public int showMinus(int a, int b) {
+		return a-b;
+	}
+	@RequestMapping("/usr/home/multi")
+	@ResponseBody
+	public int showMulti(int a, int b) {
+		return a*b;
+	}
+	@RequestMapping("/usr/home/divid")
+	@ResponseBody
+	public int showDivid(int a, int b) {
+		return a/b;
+	}
+}
