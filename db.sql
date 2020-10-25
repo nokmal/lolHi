d3c685925428437de1688a9d7c6359e2cd5af67b
nokmal <9aedarae@gmail.com>
2020-10-25 15:23:47 +0900

    DB 쿼리 설명 및 memberId DB수정

--- db.sql
+++ db.sql
@@ -1,32 +1,18 @@
+# 데이터 베이스 생성
 DROP DATABASE IF EXISTS lolHi;
 CREATE DATABASE lolHi;
 USE lolHi;
 
+# 게시물 테이블 생성
 CREATE TABLE article (
     id INT(10) UNSIGNED NOT NULL PRIMARY KEY AUTO_INCREMENT,
     regDate DATETIME NOT NULL,
-    `updateDate` DATETIME NOT NULL,
-    title CHAR(100) NOT NULL,
+    updateDate DATETIME NOT NULL,
+    title CHAR(200) NOT NULL,
     `body` TEXT NOT NULL
 );
 
-CREATE TABLE `member` (
-    id INT(10) UNSIGNED NOT NULL PRIMARY KEY AUTO_INCREMENT,    
-    regDate DATETIME NOT NULL,
-    updateDate DATETIME NOT NULL,
-    `name` CHAR(30) NOT NULL,
-    loginId CHAR(30) NOT NULL,
-    loginPw CHAR(80) NOT NULL
-);
-
-INSERT INTO `member`
-SET  regDate = NOW(),
-updateDate = NOW(),
-`name` ='guest',
-loginId='guest',
-loginPw='guest';
-
-
+# 게시물 데이터 생성
 INSERT INTO article
 SET regDate=NOW(),
 `updateDate`=NOW(),
@@ -45,3 +31,26 @@ SET regDate=NOW(),
 title='제목3',
 `body`='내용3';
 
+# 회원 테이블 생성
+CREATE TABLE `member` (
+    id INT(10) UNSIGNED NOT NULL PRIMARY KEY AUTO_INCREMENT,
+    regDate DATETIME NOT NULL,
+    updateDate DATETIME NOT NULL,
+    loginId CHAR(20) NOT NULL,
+    loginPw CHAR(100) NOT NULL,
+    `name` CHAR(100) NOT NULL
+);
+
+# 회원 생성
+INSERT INTO `member`
+SET  regDate = NOW(),
+updateDate = NOW(),
+`name` ='guest',
+loginId='guest',
+loginPw='guest';
+
+# 게시물 테이블에 memberId 칼럼 추가
+ALTER TABLE article ADD COLUMN memberId INT(10) UNSIGNED NOT NULL AFTER updateDate;
+
+# 기존 게시물들의 작성자는 1번 회원으로 지정
+UPDATE article SET memberId = 1 WHERE memberId = 0;
