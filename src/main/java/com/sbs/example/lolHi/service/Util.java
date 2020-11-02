4ea032187b7206f7269bf390e7d1f59f0c9ff99a
nokmal <9aedarae@gmail.com>
2020-11-02 11:57:13 +0900

    Add searchKeyword form and fix Util.java

--- src/main/java/com/sbs/example/lolHi/service/Util.java
+++ src/main/java/com/sbs/example/lolHi/service/Util.java
@@ -7,19 +7,21 @@ public class Util {
 	public static int getAsInt(Object object) {
 		return getAsInt(object, -1);
 	}
-	
+
 	public static int getAsInt(Object object, int defaultvalue) {
 		if (object instanceof BigInteger) {
-			return ((BigInteger)object).intValue();
-		}
-		else if (object instanceof String) {
-			return Integer.parseInt((String) object);
-		}
-		else if (object instanceof Long) {
-			return (int)((long)object);
-		}
-		else if (object instanceof Integer) {
-			return (int)object;
+			return ((BigInteger) object).intValue();
+		} else if (object instanceof String) {
+			try {
+				return Integer.parseInt((String) object);
+			} catch (NumberFormatException e) {
+				return defaultvalue;
+			}
+
+		} else if (object instanceof Long) {
+			return (int) ((long) object);
+		} else if (object instanceof Integer) {
+			return (int) object;
 		}
 		return defaultvalue;
 	}
@@ -28,9 +30,9 @@ public class Util {
 		if (object == null) {
 			return defaultValue;
 		}
-		
-		if ( object instanceof String ) {
-			return (String)(object);
+
+		if (object instanceof String) {
+			return (String) (object);
 		}
 
 		return object.toString();
