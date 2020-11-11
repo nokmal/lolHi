fd8f35309133ff3a2539fceed62f11a089eec6d2
nokmal <9aedarae@gmail.com>
2020-11-11 16:56:25 +0900

    게시물 삭제, 수정 권한을 서비스에서 검사

--- src/main/java/com/sbs/example/lolHi/service/ArticleService.java
+++ src/main/java/com/sbs/example/lolHi/service/ArticleService.java
@@ -1,6 +1,7 @@
 package com.sbs.example.lolHi.service;
 
 import java.math.BigInteger;
+import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
@@ -10,13 +11,14 @@ import org.springframework.web.bind.annotation.RequestParam;
 
 import com.sbs.example.lolHi.dao.ArticleDao;
 import com.sbs.example.lolHi.dto.Article;
+import com.sbs.example.lolHi.dto.Member;
 
 @Service
 public class ArticleService {
 	@Autowired
 	private ArticleDao articleDao;
 
-	public List<Article> getForPrintArticles(Map<String, Object> param) {
+	public List<Article> getForPrintArticles(Member actorMember, Map<String, Object> param) {
 		int page = Util.getAsInt(param.get("page"), 1);
 		
 		int itemsCountInAPage = Util.getAsInt(param.get("itemsCountInAPage"), 10);
@@ -33,7 +35,21 @@ public class ArticleService {
 		param.put("limitFrom", limitFrom);
 		param.put("limitTake", limitTake);
 		
-		return articleDao.getForPrintArticles(param);
+		List<Article> articles = articleDao.getForPrintArticles(param);
+
+		for ( Article article : articles ) {
+			if ( article.getExtra() == null ) {
+				article.setExtra(new HashMap<>()); 
+			}
+
+			boolean actorCanDelete = actorMember.getId() == article.getMemberId();
+			boolean actorCanModify = actorCanDelete;
+
+			article.getExtra().put("actorCanDelete", actorCanDelete);
+			article.getExtra().put("actorCanModify", actorCanModify);
+		}
+
+		return articles;
 	}
 
 	public Article getForPrintArticleById(int id) {
--- src/main/webapp/WEB-INF/jsp/usr/article/list.jsp
+++ src/main/webapp/WEB-INF/jsp/usr/article/list.jsp
@@ -38,11 +38,11 @@
 		내용: ${article.body}
 		<br />
 		작업 :
-		<c:if test="${loginedMemberId == article.memberId}">
+		<c:if test="${article.extra.actorCanDelete}">
 			<a onclick="if ( confirm('삭제하시겠습니까?') == false ) return false;"
 				href="doDelete?id=${article.id}">삭제</a>
 		</c:if>
-		<c:if test="${loginedMemberId == article.memberId}">
+		<c:if test="${article.extra.actorCanModify}">
 			<a href="modify?id=${article.id}">수정</a>
 		</c:if>
 		<hr />
